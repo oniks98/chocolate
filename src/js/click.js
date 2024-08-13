e7c3b562a61f485265af988a31c69ed18a3d73e2
Yurii <oniks9821@gmail.com>
2024-08-13 16:35:54 +0300

    "130824"

---
+++ src/js/click.js
@@ -0,0 +1,28 @@
+var carda = document.querySelector('.taste-item_a');
+carda.addEventListener( 'click', function() {
+  carda.classList.toggle('is-flipped');
+});
+var cardb = document.querySelector('.taste-item_b');
+cardb.addEventListener( 'click', function() {
+  cardb.classList.toggle('is-flipped');
+});
+var cardc = document.querySelector('.taste-item_c');
+cardc.addEventListener( 'click', function() {
+  cardc.classList.toggle('is-flipped');
+});
+var cardd = document.querySelector('.taste-item_d');
+cardd.addEventListener( 'click', function() {
+  cardd.classList.toggle('is-flipped');
+});
+var carde = document.querySelector('.taste-item_e');
+carde.addEventListener( 'click', function() {
+  carde.classList.toggle('is-flipped');
+});
+var cardf = document.querySelector('.taste-item_f');
+cardf.addEventListener( 'click', function() {
+  cardf.classList.toggle('is-flipped');
+});
+var cardg = document.querySelector('.taste-item_g');
+cardg.addEventListener( 'click', function() {
+  cardg.classList.toggle('is-flipped');
+});
